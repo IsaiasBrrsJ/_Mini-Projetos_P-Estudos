2d8e8b1aa73651fb7cb15591d13b1f9400810092
Isaías De Jesus Barros <88753616+IsaiasBrrsJ@users.noreply.github.com>
2022-07-05 22:25:12 -0400

    PedraPapelTesoura OOP

---
+++ Jokenpo/Classes/Game.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JokenpoReaplicando
+{
+     class Game
+    {   
+        public string jogadaMaquina { get; private set; }
+        public string jogadaUser { get; private set; }
+        internal enum Resultado
+        {
+            Ganhar, Perder, Empatar
+        }
+        internal enum Opcoes
+        {
+            Pedra, Papel, Tesoura
+        }
+
+        public Resultado jogadas(int jogador)
+        {
+            var pc = JogadaDaMaquina();
+
+            if (jogador == 1)
+            { //converter a escolha do jogador pro enum;
+                jogador = 0;
+                jogadaUser = "Pedra";
+            }
+            else if (jogador == 2)
+            {
+                jogador = 1;
+                jogadaUser = "Papel";
+            }
+            else
+            {
+                jogadaUser = "Tesoura";
+                jogador = 2;
+            }
+
+
+
+                if (jogador == pc)
+            {
+                return Resultado.Empatar;
+            }
+            else if ((jogador == (int)Opcoes.Pedra && pc == (int)Opcoes.Tesoura) || (jogador == (int)Opcoes.Tesoura && pc == (int)Opcoes.Papel) ||
+                (jogador ==(int)Opcoes.Papel && pc ==(int)Opcoes.Pedra))
+            {
+                return Resultado.Ganhar;
+            }
+            else
+            {
+                return Resultado.Perder;
+            }
+        }
+
+        private int JogadaDaMaquina()
+        {
+            var pc = DateTime.Now.Millisecond;
+
+            if(pc < 333)
+            {
+                jogadaMaquina = "Pedra";
+                return 0;
+            }
+            else if(pc > 333 && pc < 667)
+            {
+                jogadaMaquina = "Papel";
+                return 1;
+            }
+            else
+            {
+                jogadaMaquina = "Tesoura";
+                return 2;
+            }
+
+        }
+    }
+}
---
+++ Jokenpo/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading;
+
+namespace JokenpoReaplicando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            Console.ReadKey();
+        }
+
+        static void Menu()
+        {
+           
+            try
+            {
+                int opcaoJogador = -1;
+              do { 
+                Console.Clear();
+
+                
+                Console.WriteLine(Linha());
+                Console.WriteLine("  PEDRA | PAPEL | TESOURA");
+                Console.WriteLine(Linha());
+                Console.Write("\n[ 1 ] Pedra\n[ 2 ] Papel\n[ 3 ] Tesoura\n\nOpção: ");
+              
+                    opcaoJogador = int.Parse(Console.ReadLine());
+
+              } while (opcaoJogador < 1 || opcaoJogador > 3);
+
+
+                 StartGame(opcao: opcaoJogador);
+                Console.ReadKey();
+
+                Menu();
+                
+
+            }
+            catch
+            {
+                Console.Write("Opcao Inválida");
+                Thread.Sleep(1000);
+                Menu();
+            }
+
+
+        }
+
+
+        static void StartGame(int opcao)
+        {
+            var game = new Game();
+           
+            switch (game.jogadas(opcao))
+            {
+                case Game.Resultado.Ganhar:
+                    Console.WriteLine($"Você ganhou... Sua Escolha: {game.jogadaUser} | Máquina: {game.jogadaMaquina}");
+                    break;
+                case Game.Resultado.Perder:
+                    Console.WriteLine($"Você perdeu... Sua Escolha: {game.jogadaUser} | Máquina: {game.jogadaMaquina}");
+                    break;
+                case Game.Resultado.Empatar:
+                    Console.WriteLine($"Empate... Sua Escolha: {game.jogadaUser} | Máquina: {game.jogadaMaquina}");
+                    break;
+            }
+        }
+        static string Linha()
+        {
+            var linha = "";
+            for(int i = 0; i < 30; i++)
+            {
+              linha += "-";
+            }
+
+            return linha;
+
+        }
+    }
+}
