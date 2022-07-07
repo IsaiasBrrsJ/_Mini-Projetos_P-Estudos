f622f7480147725402f2a6a42207fb6a99cea3a3
Isaías De Jesus Barros <88753616+IsaiasBrrsJ@users.noreply.github.com>
2022-07-06 20:28:09 -0400

    Delete Meses.cs

--- Meses.cs
+++
@@ -1,6 +0,0 @@
-string[] meses = {"January", "February", "March", "April", "May", "June", "July",
- "August", "September", "October", "November", "December"};
-
-int mes = int.Parse(Console.ReadLine());
-
-Console.WriteLine(meses[mes - 1]);
