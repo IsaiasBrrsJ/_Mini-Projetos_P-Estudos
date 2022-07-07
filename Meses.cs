385f2e1252258e6a1a101948634cf9c89bbcff67
Isaías De Jesus Barros <88753616+IsaiasBrrsJ@users.noreply.github.com>
2022-07-06 20:24:54 -0400

    Desafio UriJudge

---
+++ Meses.cs
@@ -0,0 +1,6 @@
+string[] meses = {"January", "February", "March", "April", "May", "June", "July",
+ "August", "September", "October", "November", "December"};
+
+int mes = int.Parse(Console.ReadLine());
+
+Console.WriteLine(meses[mes - 1]);
