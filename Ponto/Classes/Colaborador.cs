b705f8eb34bd5c6ee0c350c5b4d2530021f01e23
Isaías De Jesus Barros <88753616+IsaiasBrrsJ@users.noreply.github.com>
2022-07-22 19:46:24 -0400

    Ponto Eletrônico
    Feito em Windows forms, o mesmo registra a entrada e saída, realizado em prol de aprendizado

---
+++ Ponto/Classes/Colaborador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace RegistrarPonto
+{
+    class Colaborador
+    {
+        public string nome { get; set; }
+        public int matricula { get; set; }
+        public string senha { get; set; }
+
+        public DateTime? entrada { get; set; }
+        public DateTime? saida { get; set; }
+
+        public bool? logado { get; set; }
+    }
+}
---
+++ Ponto/Classes/VerificaColaborador.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace RegistrarPonto
+{
+    class VerificaColaborador
+    {
+
+        private Colaborador _logado = null;
+
+        private Colaborador[] colaboradores =
+        {
+           new Colaborador() {nome = "Isaias", matricula = 111, senha = "teste123"},
+           new Colaborador() {nome = "Barros Jesus", matricula = 333, senha = "333"}
+        };
+
+        public Colaborador LOGADO
+        {
+            get => _logado;
+            private set
+            {
+                _logado = value;
+            }
+        }
+
+        public string Log(int matricula, string senha)
+        {
+            foreach (Colaborador colaborador in colaboradores)
+            {
+              if((colaborador.matricula.Equals(matricula) && colaborador.senha.Equals(senha)) && colaborador.logado is null)
+                {
+                    colaborador.logado = true;
+                    LOGADO = colaborador;
+                    colaborador.entrada = DateTime.Now;
+                    return "Entrou";
+                }
+                else if((colaborador.matricula.Equals(matricula) && colaborador.senha.Equals(senha)) && (colaborador.logado is true))
+                {
+                    colaborador.logado = null;
+                    LOGADO = colaborador;
+                    colaborador.saida = DateTime.Now;
+                    return "Saiu";
+                }
+            }
+
+            return "";
+        }
+      
+    }
+}
